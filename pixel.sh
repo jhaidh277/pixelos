0ab5dd702f890efff8dc6f78a1a7c1fd9f37cded
jihad <jhaidh277@gmail.com>
2026-05-27 17:10:23 +0600

    Initialize PixelOS build environment in pixel.sh

--- pixel.sh
+++ pixel.sh
@@ -1,1 +1,13 @@
+#!/bin/bash
 
+rm -rf .repo/local_manifests
+
+repo init -u https://github.com/PixelOS-AOSP/android_manifest.git -b sixteen-qpr2 --git-lfs
+git clone https://github.com/mdnoyon80123/hotdogb_local_manifest --depth 1 -b inf-q2 .repo/local_manifests
+
+/opt/crave/resync.sh
+
+source build/envsetup.sh
+breakfast hotdogb userdebug
+
+m pixelos
