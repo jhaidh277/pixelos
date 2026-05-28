5fcc65a646cb27d9e373be4a9ded09211a9f9928
jihad <jhaidh277@gmail.com>
2026-05-29 00:47:29 +0600

    Update pixel.sh

--- pixel.sh
+++ pixel.sh
@@ -8,5 +8,9 @@ git clone https://github.com/mdnoyon80123/hotdogb_local_manifest --depth 1 -b in
 /opt/crave/resync.sh
 
 source build/envsetup.sh
-lunch pixelos_hotdogb-trunk_staging-userdebug
+
+export TARGET_RELEASE=$(ls build/release/trunks/ 2>/dev/null | head -n 1 | cut -d. -f1)
+[ -z "$TARGET_RELEASE" ] && export TARGET_RELEASE="trunk_staging"
+
+lunch pixelos_hotdogb-${TARGET_RELEASE}-userdebug
 m pixelos
